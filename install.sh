d5f3f8cafa46e9976eda3779a4424e047f446dd3
Andrew Shelestov <streamx3@gmail.com>
2016-05-25 11:52:51 +0300

    + Added  PS1 variable to .bashrc + Added mc aliase for skin

--- install.sh
+++ install.sh
@@ -44,6 +44,18 @@ set statusline+=%F
 set laststatus=2
 \" >> /etc/vim/vimrc"
 
+
+printf "${GREEN}Patching users .bashrc...${NC}\n"
+echo -e "export PS1=\"\[\033[38;5;2m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \w \[$(tput sgr0)\]\[\033[38;5;2m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\"
+alias mc=\"mc -S dark\"
+" >> ~/.bashrc
+
+
+printf "${GREEN}Patching root's .bashrc...${NC}\n"
+sudo su -s "echo -e \"export PS1=\"\[\033[38;5;1m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\] \w \[$(tput sgr0)\]\[\033[38;5;1m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\"
+alias mc=\"mc -S dark\"
+" >> /root/.bashrc"
+
 printf "${GREEN}Adding current user to dialout group...${NC}\n"
 sudo adduser `whoami` dialout
 
